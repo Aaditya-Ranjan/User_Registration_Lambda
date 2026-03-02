513cca825b9819c4f076ff5b8b38fe2e4a2b9f88
Aaditya Ranjan <ranjanaadi50@gmail.com>
2026-03-02 11:00:16 +0530

    [Aaditya] Added: UC3: Validate Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,16 +8,26 @@ public class UserRegistration {
         return lastName.matches("[A-Z][a-zA-Z]{2,}");
     }
 
+    public boolean validateEmail(String email) {
+        return email.matches("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+[.][a-zA-Z0-9]{2,}([.][a-zA-Z]{2,})?$");
+    }
+
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
 
         // UC1 - First Name
-        System.out.println(user.validateFirstName("John"));   // true
-        System.out.println(user.validateFirstName("jo"));     // false
+        System.out.println(user.validateFirstName("John"));        // true
+        System.out.println(user.validateFirstName("jo"));          // false
 
         // UC2 - Last Name
-        System.out.println(user.validateLastName("Doe"));     // true
-        System.out.println(user.validateLastName("do"));      // false
-        System.out.println(user.validateLastName("doe"));     // false
+        System.out.println(user.validateLastName("Doe"));          // true
+        System.out.println(user.validateLastName("do"));           // false
+
+        // UC3 - Email
+        System.out.println(user.validateEmail("abc@bl.co.in"));    // true
+        System.out.println(user.validateEmail("abc.xyz@bl.co.in")); // true
+        System.out.println(user.validateEmail("abc@bl.co"));       // true
+        System.out.println(user.validateEmail("abc@.co.in"));      // false
+        System.out.println(user.validateEmail("abc@bl..in"));      // false
     }
 }
