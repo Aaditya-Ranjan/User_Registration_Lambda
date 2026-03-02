d1c191cc521c787ae6eb3e9fe3a4aaf83b01f114
Aaditya Ranjan <ranjanaadi50@gmail.com>
2026-03-02 10:57:15 +0530

    [Aaditya] Added: UC2: Validate Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,10 +4,20 @@ public class UserRegistration {
         return firstName.matches("[A-Z][a-zA-Z]{2,}");
     }
 
+    public boolean validateLastName(String lastName) {
+        return lastName.matches("[A-Z][a-zA-Z]{2,}");
+    }
+
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
+
+        // UC1 - First Name
         System.out.println(user.validateFirstName("John"));   // true
         System.out.println(user.validateFirstName("jo"));     // false
-        System.out.println(user.validateFirstName("john"));   // false
+
+        // UC2 - Last Name
+        System.out.println(user.validateLastName("Doe"));     // true
+        System.out.println(user.validateLastName("do"));      // false
+        System.out.println(user.validateLastName("doe"));     // false
     }
 }
