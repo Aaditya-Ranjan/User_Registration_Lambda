8766f02f4423c86fded4c91f36f38c0685438044
Aaditya Ranjan <ranjanaadi50@gmail.com>
2026-03-02 11:38:24 +0530

    [Aaditya] Added: UC13: Refactor to use Lambda Functions for Validation

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,59 +1,61 @@
 package com.userregistration;
 
+import java.util.function.Function;
+
 public class UserRegistration {
 
-    public boolean validateFirstName(String firstName) {
+    Function<String, Boolean> validateFirstName = firstName -> {
         if (firstName.matches("[A-Z][a-zA-Z]{2,}"))
             return true;
         throw new UserRegistrationException(
                 UserRegistrationException.ExceptionType.INVALID_FIRST_NAME,
                 "Invalid First Name: " + firstName
         );
-    }
+    };
 
-    public boolean validateLastName(String lastName) {
+    Function<String, Boolean> validateLastName = lastName -> {
         if (lastName.matches("[A-Z][a-zA-Z]{2,}"))
             return true;
         throw new UserRegistrationException(
                 UserRegistrationException.ExceptionType.INVALID_LAST_NAME,
                 "Invalid Last Name: " + lastName
         );
-    }
+    };
 
-    public boolean validateEmail(String email) {
+    Function<String, Boolean> validateEmail = email -> {
         if (email.matches("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+[.][a-zA-Z0-9]{2,}([.][a-zA-Z]{2,})?$"))
             return true;
         throw new UserRegistrationException(
                 UserRegistrationException.ExceptionType.INVALID_EMAIL,
                 "Invalid Email: " + email
         );
-    }
+    };
 
-    public boolean validateMobile(String mobile) {
+    Function<String, Boolean> validateMobile = mobile -> {
         if (mobile.matches("^[0-9]{2} [0-9]{10}$"))
             return true;
         throw new UserRegistrationException(
                 UserRegistrationException.ExceptionType.INVALID_MOBILE,
                 "Invalid Mobile: " + mobile
         );
-    }
+    };
 
-    public boolean validatePassword(String password) {
+    Function<String, Boolean> validatePassword = password -> {
         if (password.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$"))
             return true;
         throw new UserRegistrationException(
                 UserRegistrationException.ExceptionType.INVALID_PASSWORD,
                 "Invalid Password: " + password
         );
-    }
+    };
 
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
 
-        System.out.println(user.validateFirstName("John"));
-        System.out.println(user.validateLastName("Doe"));
-        System.out.println(user.validateEmail("abc@bl.co.in"));
-        System.out.println(user.validateMobile("91 9919819801"));
-        System.out.println(user.validatePassword("Password1@"));
+        System.out.println(user.validateFirstName.apply("John"));
+        System.out.println(user.validateLastName.apply("Doe"));
+        System.out.println(user.validateEmail.apply("abc@bl.co.in"));
+        System.out.println(user.validateMobile.apply("91 9919819801"));
+        System.out.println(user.validatePassword.apply("Password1@"));
     }
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -12,81 +12,81 @@ public class UserRegistrationTest {
     // UC1 - First Name
     @Test
     public void givenValidFirstName_ShouldReturnTrue() {
-        assertTrue(user.validateFirstName("John"));
+        assertTrue(user.validateFirstName.apply("John"));
     }
 
     @Test
     public void givenInvalidFirstName_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validateFirstName("jo"));
+        assertThrows(UserRegistrationException.class, () -> user.validateFirstName.apply("jo"));
     }
 
     @Test
     public void givenFirstNameWithNoCapital_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validateFirstName("john"));
+        assertThrows(UserRegistrationException.class, () -> user.validateFirstName.apply("john"));
     }
 
     // UC2 - Last Name
     @Test
     public void givenValidLastName_ShouldReturnTrue() {
-        assertTrue(user.validateLastName("Doe"));
+        assertTrue(user.validateLastName.apply("Doe"));
     }
 
     @Test
     public void givenInvalidLastName_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validateLastName("do"));
+        assertThrows(UserRegistrationException.class, () -> user.validateLastName.apply("do"));
     }
 
     @Test
     public void givenLastNameWithNoCapital_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validateLastName("doe"));
+        assertThrows(UserRegistrationException.class, () -> user.validateLastName.apply("doe"));
     }
 
     // UC3 - Email
     @Test
     public void givenValidEmail_ShouldReturnTrue() {
-        assertTrue(user.validateEmail("abc@bl.co.in"));
+        assertTrue(user.validateEmail.apply("abc@bl.co.in"));
     }
 
     @Test
     public void givenInvalidEmail_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validateEmail("abc@.co.in"));
+        assertThrows(UserRegistrationException.class, () -> user.validateEmail.apply("abc@.co.in"));
     }
 
     // UC4 - Mobile
     @Test
     public void givenValidMobile_ShouldReturnTrue() {
-        assertTrue(user.validateMobile("91 9919819801"));
+        assertTrue(user.validateMobile.apply("91 9919819801"));
     }
 
     @Test
     public void givenInvalidMobile_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validateMobile("9919819801"));
+        assertThrows(UserRegistrationException.class, () -> user.validateMobile.apply("9919819801"));
     }
 
     // UC5-UC8 - Password
     @Test
     public void givenValidPassword_ShouldReturnTrue() {
-        assertTrue(user.validatePassword("Password1@"));
+        assertTrue(user.validatePassword.apply("Password1@"));
     }
 
     @Test
     public void givenPasswordWithNoUpperCase_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validatePassword("password1@"));
+        assertThrows(UserRegistrationException.class, () -> user.validatePassword.apply("password1@"));
     }
 
     @Test
     public void givenPasswordWithNoNumber_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validatePassword("Password@"));
+        assertThrows(UserRegistrationException.class, () -> user.validatePassword.apply("Password@"));
     }
 
     @Test
     public void givenPasswordWithNoSpecialChar_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validatePassword("Password1"));
+        assertThrows(UserRegistrationException.class, () -> user.validatePassword.apply("Password1"));
     }
 
     @Test
     public void givenPasswordWithLessThan8Chars_ShouldThrowException() {
-        assertThrows(UserRegistrationException.class, () -> user.validatePassword("Pass1@"));
+        assertThrows(UserRegistrationException.class, () -> user.validatePassword.apply("Pass1@"));
     }
 
     // UC11 - Parameterised Email Test
@@ -99,7 +99,7 @@ public class UserRegistrationTest {
             "abc123@bl.co.in, true"
     })
     public void givenValidEmailSamples_ShouldReturnTrue(String email, boolean expected) {
-        assertEquals(expected, user.validateEmail(email));
+        assertEquals(expected, user.validateEmail.apply(email));
     }
 
     @ParameterizedTest
@@ -111,6 +111,6 @@ public class UserRegistrationTest {
             "abc@bl@co.in"
     })
     public void givenInvalidEmailSamples_ShouldThrowException(String email) {
-        assertThrows(UserRegistrationException.class, () -> user.validateEmail(email));
+        assertThrows(UserRegistrationException.class, () -> user.validateEmail.apply(email));
     }
 }
